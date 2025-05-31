06d8c5f7c67b2caa32060028e917ae95dff01e23
dszturm <szturm.david@gmail.com>
2025-05-31 16:59:41 -0300

    ratings

--- src/contractor/contractor.module.ts
+++ src/contractor/contractor.module.ts
@@ -3,10 +3,12 @@ import { ContractorService } from './contractor.service';
 import { ContractorController } from './contractor.controller';
 import { contractorProviders } from './contractor.providers';
 import { DatabaseModule } from 'src/database/database.module';
+import { AuthService } from 'src/auth/auth.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ContractorController],
-  providers: [ContractorService,...contractorProviders],
-  imports:[DatabaseModule]
+  providers: [ContractorService,AuthService,...contractorProviders],
+  imports:[DatabaseModule,AuthModule]
 })
 export class ContractorModule {}
