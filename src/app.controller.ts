c24c74b4b7905895e9c1a0acca2f9cfeb117e0d4
dszturm <szturm.david@gmail.com>
2025-06-03 19:42:19 -0300

    file_upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { FileInterceptor } from '@nestjs/platform-express';
+const MAX_PROFILE_PICTURE_SIZE_IN_BYTES = 2 * 1024 * 1024;
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  public async uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({ fileType: 'image/jpeg' })
+        .addMaxSizeValidator({ maxSize: MAX_PROFILE_PICTURE_SIZE_IN_BYTES })
+        .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
+    )
+    file,
+  ) {
+    return file;
   }
 }
---
+++ src/shared/imgUpload.service.ts
@@ -0,0 +1,35 @@
+import * as nodemailer from "nodemailer";
+
+export class EmailService {
+transporter: any;
+
+constructor() {
+    this.transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_APP_PASSWORD,
+        },
+    });
+}
+
+
+async sendEmail(to: string, subject: string, html: string) {
+    try {
+        const mailOptions = {
+            from: process.env.GMAIL_USER,
+            to,
+            subject,
+            html,
+        };
+
+        await this.transporter.sendMail(mailOptions);
+        return true;
+    } catch (error) {
+        console.error('Error sending email:', error);
+        throw error;
+    }
+}
+
+
+}
