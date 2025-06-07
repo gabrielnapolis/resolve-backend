bccb28ece0befe334e434e19ef4a6c5b8ed92f3d
dszturm <szturm.david@gmail.com>
2025-06-07 12:28:03 -0300

    merge

--- src/works/entities/work.entity.ts
+++ src/works/entities/work.entity.ts
@@ -9,39 +9,7 @@ import {
   Entity,
 } from 'typeorm';
 @Entity()
-<<<<<<< HEAD
+
 export class Work { 
 
-=======
-export class Work {
-  @PrimaryGeneratedColumn()
-  id: string;
-
-  @Column({
-    nullable: true,
-  })
-  description: string;
-
-  @Column({
-    nullable: true,
-  })
-  state: string;
-
-  @Column({
-    nullable: true,
-  })
-  city: string;
-
-  @ManyToMany(
-    (type) => ContractorSpeciality,
-    (speciality) => speciality.contractor,
-  )
-  specialities: ContractorSpeciality[];
-
-  @ManyToOne((type) => Contractor, (contractor) => contractor.works)
-  contractor: Contractor;
-
-  @ManyToOne((type) => Client, (client) => client.works)
-  client: Client;
->>>>>>> 2365b36b0b5a00a4802e86e8d54f15f86ce1c762
 }
